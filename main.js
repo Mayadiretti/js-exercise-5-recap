70d2eee27dca09bd659451228272625ae6503df7
maya diretti <maya.diretti@icloud.com>
2023-11-01 20:55:37 +0100

    feat / exercise 1

---
+++ main.js
@@ -0,0 +1,10 @@
+let question = prompt('dimmi una parola');
+
+function stringReverse(string) {
+    let reversedString = "";
+    for(let i = string.lenght -1; i >= 0; i--){
+        reversedString - reversedString + string[i];
+    }
+
+}
+console.log(reversedString);
